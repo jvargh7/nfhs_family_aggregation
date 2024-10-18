3545de996bc3cb4eabfd7626631e593678b7435c
Varghese <JVARGH7@emory.edu>
2024-10-18 00:13:05 -0400

    Updated nfapre02b with edits for o_htn_blood_related and o_htn_not_blood_related to account for differences by individual's relationship TO head

--- preprocessing/nfapre02b_creating individual and household datasets with alternative spousal specification.R
+++ preprocessing/nfapre02b_creating individual and household datasets with alternative spousal specification.R
@@ -45,44 +45,30 @@ all_adults <- bind_rows(iapr7e_female_cleaned, iapr7e_male_cleaned) %>%
       relationship_hh_head == 16 ~ "Niece/nephew",
       relationship_hh_head == 17 ~ "Domestic servant"
     )
-  )
-
-# Step 1: Identify households where the spouse has hypertension
-hh_with_spouse_htn <- all_adults %>%
-  group_by(cluster, hhid) %>%
-  summarize(spouse_hh_htn = sum(htn_disease == 1 & relationship_hh_head == "Wife or husband", na.rm = TRUE)) %>%
-  ungroup()
-
-# Step 2: Update the blood_relation for children and grandchildren
-all_adults <- all_adults %>%
-  left_join(hh_with_spouse_htn, by = c("cluster", "hhid")) %>%
+  ) %>%
+  # Step 1: Classify relationships as 'blood_relation = 0" or "blood_relation = 1"
+  
   mutate(
     blood_relation = case_when(
       relationship_hh_head == "Head" ~ "1",  # Household head is consanguineal
       relationship_hh_head == "Wife or husband" ~ "0",  # Spouse is affinal to household head
-      relationship_hh_head %in% c("Son/daughter", "Grandchild") &
-        spouse_hh_htn > 0 & htn_disease == 1 ~ "1",  # Spouse's biological children/grandchildren are consanguineal if spouse has hypertension
       relationship_hh_head %in% c("Son/daughter", "Grandchild", "Parent", "Brother/sister", "Niece/nephew by blood") ~ "1",  # Other consanguineal relations
       TRUE ~ "0"  # All others are non-consanguineal
     )
   )
 
-# Calculate total hypertension counts for blood-related and non-blood-related members at the household level
+
+# Step 2. Calculate total hypertension counts for blood-related and non-blood-related members at the household level
 hh_iapr <- all_adults %>%
   left_join(eligible_adults %>% select(cluster, hhid, linenumber) %>% mutate(is_eligible = 1), 
             by = c("cluster", "hhid", "linenumber")) %>%
   group_by(cluster, hhid) %>%
   mutate(
     # Create spouse_hh_htn variable: 1 if the spouse has hypertension, 0 otherwise
+    head_hh_htn = sum(htn_disease == 1 & relationship_hh_head == "Head", na.rm = TRUE),
     spouse_hh_htn = sum(htn_disease == 1 & relationship_hh_head == "Wife or husband", na.rm = TRUE),
-    # Adjust hypertension counts for blood-related and non-blood-related members,
-    # considering spouse's biological children or grandchildren as consanguineal
-    total_htn_blood_related = sum(htn_disease == 1 & blood_relation == "1", na.rm = TRUE) + 
-      ifelse(spouse_hh_htn > 0 & sum(htn_disease == 1 & blood_relation == "0", na.rm = TRUE) > 0, 1, 0),  # Count spouse's biological children/grandchildren as consanguineal if spouse has hypertension
-    total_htn_not_blood_related = sum(htn_disease == 1 & blood_relation == "0", na.rm = TRUE) - 
-      ifelse(spouse_hh_htn > 0 & sum(htn_disease == 1 & blood_relation == "0", na.rm = TRUE) > 0, 1, 0)  # Adjust non-blood-related counts accordingly
-  ) %>%
-  ungroup() %>%
+    kids_hh_htn = sum(htn_disease == 1 & relationship_hh_head %in% c("Son/daughter", "Grandchild"), na.rm = TRUE)) %>% 
+    ungroup() %>%
   group_by(cluster, hhid) %>%
   summarize(
     nmembers = max(nmembers),
@@ -100,11 +86,21 @@ hh_iapr <- all_adults %>%
     prop_htn = mean(htn_disease, na.rm = TRUE),
     n_htn_blood_related = sum(htn_disease == 1 & blood_relation == "1", na.rm = TRUE),
     n_htn_not_blood_related = sum(htn_disease == 1 & blood_relation == "0", na.rm = TRUE),
-    total_htn_blood_related = first(total_htn_blood_related),
-    total_htn_not_blood_related = first(total_htn_not_blood_related),
-    spouse_hh_htn = max(spouse_hh_htn, na.rm = TRUE)  # Add spouse hypertension to the summary
+    head_hh_htn = max(head_hh_htn,na.rm=TRUE),
+    spouse_hh_htn = max(spouse_hh_htn, na.rm = TRUE),  # Add spouse hypertension to the summary
+    kids_hh_htn = max(kids_hh_htn,na.rm=TRUE) # Add kids hypertension to the summary
   ) %>%
   mutate(htn_ge2 = case_when(n_valid == 0 ~ NA_real_, n_htn >= 2 ~ 1, n_htn < 2 ~ 0))
+
+
+
+# Step 3: Identify households where the spouse has hypertension
+hh_with_spouse_htn <- all_adults %>%
+  group_by(cluster, hhid) %>%
+  summarize(spouse_hh_htn = sum(htn_disease == 1 & relationship_hh_head == "Wife or husband", na.rm = TRUE),
+            kids_hh_htn = sum(htn_disease ==1 & relationship_hh_head %in% c("Son/daughter", "Grandchild"), na.rm = TRUE)) %>%
+  ungroup()
+
 
 # Create final dataset with analytic sample
 all_adults_analytic_sample <- all_adults %>%
@@ -124,17 +120,58 @@ all_adults_analytic_sample <- all_adults %>%
     bp_group = factor(bp_group, levels = c(1:4), labels = c("<120/80", "<140/90", "<160/100", ">=160/100")),
     o_htn = n_htn - htn_disease,
     o_diagnosedhtn = n_diagnosedhtn - htn_diagnosed,
-    htn_undiagnosed = 1 - htn_diagnosed,
+    htn_undiagnosed = case_when(htn_disease == 1 ~ 1 - htn_diagnosed,
+                                TRUE ~ NA_real_),
     o_undiagnosedhtn = o_htn - o_diagnosedhtn,
-    o_htn_blood_related = total_htn_blood_related - ifelse(htn_disease == 1 & blood_relation == "1", 1, 0),
-    o_htn_not_blood_related = total_htn_not_blood_related - ifelse(htn_disease == 1 & blood_relation == "0", 1, 0),
     cluster_hhid = paste0(sprintf("%05d", cluster), sprintf("%03d", hhid))
   ) %>%
+  # Create variables for counts of blood relation
+  # Adjust hypertension counts for blood-related and non-blood-related members,
+  # considering spouse's biological children or grandchildren as consanguineal
+    
+  
+  # n_htn_blood_related: How many people related to the head have htn?
+  # n_htn_not_blood_related: How many people not related to the head have htn?
+  
+  # ind_htn_blood_related: For each individual, how many people are consanguineal to them?
+  # ind_htn_not_blood_related: For each individual, how many people are affinal to them?
+  mutate(ind_htn_blood_related = case_when(relationship_hh_head %in% c("Head","Son/daughter", "Grandchild", "Parent", "Brother/sister", "Niece/nephew by blood") ~ n_htn_blood_related,
+                                           TRUE ~ 0),
+         ind_htn_not_blood_related = case_when(relationship_hh_head %in% c("Head","Son/daughter", "Grandchild", "Parent", "Brother/sister", "Niece/nephew by blood") ~ n_htn_not_blood_related,
+                                               TRUE ~ n_htn_blood_related + n_htn_not_blood_related)
+         ) %>% 
+  
+  # adjustment_htn_blood_related: What do you need to adjust ind_htn_blood_related with
+  # adjustment_htn_not_blood_related: What do you need to adjust ind_htn_not_blood_related with
+  mutate(adjustment_htn_blood_related = case_when(relationship_hh_head %in% c("Son/daughter", "Grandchild") ~ spouse_hh_htn,
+                                                  relationship_hh_head %in% c("Wife or husband") ~ kids_hh_htn,
+                                                  TRUE ~ 0),
+         adjustment_htn_not_blood_related = case_when(relationship_hh_head %in% c("Son/daughter", "Grandchild") ~ -spouse_hh_htn,
+                                                 relationship_hh_head %in% c("Wife or husband") ~ -kids_hh_htn,
+                                                 TRUE ~ 0)) %>% 
+  mutate(
+    # Consanguineal + Adjustment - Own Status accounting for blood relationship 
+    o_htn_blood_related = ind_htn_blood_related + adjustment_htn_blood_related - ifelse(htn_disease == 1 & blood_relation == "1", 1, 0),
+    # Affinal + Adjustment - Own Status accounting for blood relationship 
+    o_htn_not_blood_related = ind_htn_not_blood_related + adjustment_htn_not_blood_related - ifelse(htn_disease == 1 & blood_relation == "0", 1, 0)
+    
+  ) %>% 
   dplyr::filter(!is.na(htn_disease)) %>%
   arrange(cluster, hhid, linenumber)
 
-# Save the results
-saveRDS(all_adults, paste0(path_family_aggregation_folder, "/working/cleaned/nfapre02_all_adults.RDS"))
-saveRDS(total_adults, paste0(path_family_aggregation_folder, "/working/cleaned/nfapre02_total_adults.RDS"))
-saveRDS(hh_iapr, paste0(path_family_aggregation_folder, "/working/cleaned/nfapre02_hh_iapr.RDS"))
-saveRDS(all_adults_analytic_sample, paste0(path_family_aggregation_folder, "/working/cleaned/nfapre02_all_adults_analytic_sample.RDS"))
+
+all_adults_analytic_sample %>% dplyr::select(cluster_hhid,spouse_hh_htn,kids_hh_htn,relationship_hh_head,
+                                                  htn_disease,htn_diagnosed,htn_undiagnosed,
+                                                  n_htn,n_htn_blood_related, n_htn_not_blood_related, 
+                                                  ind_htn_blood_related, ind_htn_not_blood_related, 
+                                                  adjustment_htn_blood_related,adjustment_htn_not_blood_related,
+                                                  o_htn,o_diagnosedhtn,o_undiagnosedhtn,o_htn_blood_related,o_htn_not_blood_related) %>% 
+       # head(n = 10000) %>% 
+       dplyr::filter(cluster_hhid == "00125017") %>% 
+  View()
+
+# # Save the results
+# saveRDS(all_adults, paste0(path_family_aggregation_folder, "/working/cleaned/nfapre02_all_adults.RDS"))
+# saveRDS(total_adults, paste0(path_family_aggregation_folder, "/working/cleaned/nfapre02_total_adults.RDS"))
+# saveRDS(hh_iapr, paste0(path_family_aggregation_folder, "/working/cleaned/nfapre02_hh_iapr.RDS"))
+# saveRDS(all_adults_analytic_sample, paste0(path_family_aggregation_folder, "/working/cleaned/nfapre02_all_adults_analytic_sample.RDS"))
